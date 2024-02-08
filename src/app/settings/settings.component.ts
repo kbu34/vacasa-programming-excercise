38e5a9c05b57e77697026678995a5c6eb07bd454
Phillip Kim <phillipkim156@gmail.com>
2024-02-08 17:06:24 +1300

    cleanup

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
